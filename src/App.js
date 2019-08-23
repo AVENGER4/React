67746ff069def88b31912497a7773b793dde748f
AVENGER4 <gadiaabhishek@gmail.com>
2019-08-23 23:40:07 +0530

    first commit on git

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+
+    state = {
+      persons: [ 
+        {name: 'MAx', age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'KAllu', age: 26}
+      ]
+    }
+    render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am learning React App</h1>
+        <button>Switch Name</button>
+        <Person  name = {this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name = {this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: racing</Person>
+        <Person name = {this.state.persons[2].name} age={this.state.persons[2].age}/>
+
+      </div>
+    );
+   // return React.createElement('div',{className:'App'}, React.createElement('h1', null, 'Does it work nows!'));
+  }
+}
+
+export default App;
